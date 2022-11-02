abd3fed33dc3685169538ffb548d655d6ea38b13
salihkertik <salihkertik1453@gmail.com>
2022-11-02 19:30:24 +0300

    Flight Ticket Reservation Automation
    Flight Ticket Reservation Automation

---
+++ Flight_Ticket_Reservation/Flight_Ticket_Reservation/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flight_Ticket_Reservation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota: " + comboBox1.Text + " - " + comboBox2.Text + " Tarih: "+dateTimePicker1+ "Saat: "+maskedTextBox1.Text + "Yolcu Bilgileri ~~ Ad:"+textBox1.Text+"TC: "+maskedTextBox2.Text + " Telefon: " + maskedTextBox3.Text);
+            MessageBox.Show("Yolcu Kaydı Yapıldı.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label8.Text = comboBox2.Text;
+            comboBox2.Text = comboBox1.Text;
+            comboBox1.Text = label8.Text;
+        }
+    }
+}
